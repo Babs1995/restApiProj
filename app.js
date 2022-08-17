be097688f00ec5b35f30a7a3a35df350fdef13b7
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-17 14:06:54 -0700

    fixed routes

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ const routes = require('./routes');
   }
 })();
 
-app.use('/api', routes);
-
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+app.use('/api', routes);
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
