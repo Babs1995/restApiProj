c0f2c6f6b95b751599bb9796a1a3beeb64807e40
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-16 23:37:06 -0700

    setting up asynch handler in routes

--- routes.js
+++ routes.js
@@ -4,6 +4,15 @@ const auth = require('basic-auth');
 const { authUser } = require('./middleware/auth-user');
 const { User, Courses } = require('./models');
 const { asyncHandler } = require('./middleware/asyncHandler');
+const router = express.Router();
 
-
+function asyncHandler(cb){
+    return async (req, res, next) => {
+      try {
+        await cb(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    }
+  }
 
