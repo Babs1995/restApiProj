51868296e50094b023f69c9080a20c435fe96f98
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-17 12:03:34 -0700

    if else statements in authuser set up and working

---
+++ middleware/auth-user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require('bcrypt')
+const auth = require('basic-auth');
+
+exports.authUser = async (req, res, next) => {
+    let message;
+    const credentials = auth(req);
+
+    if (credentials) {
+        const user = await User.findOne({ 
+            where: { 
+                emailAddress: credentials.name },
+        });
+    
+        if (user) {
+          const authenticated = bcrypt.compareSync(credentials.pass, user.password);
+    
+          if (authenticated) {
+            console.log(`Authentication successful for user: ${user.emailAddress}`);
+            req.currentUser = user;
+          } else {
+            message = `Authentication failure for user: ${user.emailAddress}`;
+          }
+        } else {
+          message = `User not found for user: ${credentials.emailAddress}`;
+        }
+      } else {
+        message = "Auth header not found";
+      }
+      if (message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' });
+      } else {
+        next();
+      }
+    };
